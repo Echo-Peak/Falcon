98059481d100edd9915c1f2da754861d81ddc328
echo-peak <echopeakdev@gmail.com>
2016-07-15 00:11:39 -0400

    more reliable logic

--- src/modules/namespace/local-storage-setup.js
+++ src/modules/namespace/local-storage-setup.js
@@ -1,15 +1,14 @@
 export default function localStorageSetup(){
-  if(!localStorage.lastGroup){
+  if(localStorage.lastGroup === void 0 || localStorage.lastGroup === '' || localStorage.lastGroup ==null){
     localStorage.lastGroup ='default'
   }
-  if(!localStorage.groups){
+  if(localStorage.groups === void 0 || localStorage.groups === '' || localStorage.groups == null){
     localStorage.groups = '["default"]'
   }
-  if(!sessionStorage.deleted){
+  if(sessionStorage.deleted === void 0 || sessionStorage.deleted === '' || sessionStorage.deleted ==null){
     sessionStorage.deleted = '[]'
   }
-  if(!sessionStorage.groups){
+  if(sessionStorage.groups === void 0 || sessionStorage.groups === '' || sessionStorage.groups ==null){
     sessionStorage.groups = '[]'
   }
-
 }
